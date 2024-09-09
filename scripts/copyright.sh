27c1769c3aba3a18b81c9a6fc09027c3ce23500d
Jan Pokorný <jenompokorny@gmail.com>
2024-09-09 13:03:53 +0200

    fix: remove non-existent folder from copyright config

--- scripts/copyright.sh
+++ scripts/copyright.sh
@@ -24,9 +24,9 @@ if ! command -v nwa &> /dev/null && command -v go &> /dev/null; then
 fi
 
 if command -v nwa &> /dev/null; then
-  nwa add -l apache -c "IBM Corp." scripts executor ci test src examples
+  nwa add -l apache -c "IBM Corp." scripts executor test src examples
 elif command -v docker &> /dev/null; then
-  docker run -it -v "${PWD}:/src" ghcr.io/b1nary-gr0up/nwa:main add -l apache -c "IBM Corp." scripts executor ci test src examples
+  docker run -it -v "${PWD}:/src" ghcr.io/b1nary-gr0up/nwa:main add -l apache -c "IBM Corp." scripts executor test src examples
 else
   echo "Error: 'nwa' is not available. Either install it manually or install go/docker."
   exit 1
